ff90bf8b12d93afbb2b4b4beefa99c386a2ae16a
maxi67 <gckc3130@gmail.com>
2016-05-09 12:23:43 +0800

    Update timerSample.ino

--- timerSample.ino
+++ timerSample.ino
@@ -5,7 +5,9 @@ Timer t2;
 void setup() {
   
   pinMode(2, OUTPUT); // Pin: Output Mode
-  t2.oscillate(2, 1200, HIGH); // 以oscillate設定每幾毫秒切換一次狀態(方波)(參數:第幾腳/週期幾毫秒/初始狀態)
+  
+  // 以oscillate設定每幾毫秒切換一次狀態(方波) 參數:第幾腳/週期幾毫秒/初始狀態
+  t2.oscillate(2, 1200, HIGH); 
   
 }
 
